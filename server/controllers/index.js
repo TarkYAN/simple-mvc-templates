5f0fc9b921bd10fe330ad2ca99f823d7d77788c2
LordOden <butter2002at@gmail.com>
2024-10-21 14:51:15 -0400

    added Oct21

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,11 +3,21 @@
 let name = 'unknown';
 
 const hostIndex = (req, res) => {
-
+  return res.render('index', {
+    title: 'Home',
+    pageName: 'Home Page',
+    userName: name,
+  });
 };
 
 const hostPage1 = (req, res) => {
+  const myArray = ['Austin', 'Cody', 'Erika', 'Eric'];
+  const filtered = myArray.filter(x => x.length <= 5);
 
+  return res.render('page1', {
+    title: 'Page 1',
+    names: filtered,
+  });
 };
 
 const hostPage2 = (req, res) => {
@@ -15,15 +25,25 @@ const hostPage2 = (req, res) => {
 };
 
 const getName = (req, res) => {
-
+  return res.json({name});
 };
 
 const setName = (req, res) => {
+  if(!req.body.firstname || !req.body.lastname){
+    return res.status(400).json({
+      error: 'firstname and lastname are both required',
+      id: 'setNameMissingParams',
+    });
+  }
 
+  name = `${req.body.firstname} ${req.body.lastname}`;
+  return res.json({name});
 };
 
 const notFound = (req, res) => {
-
+  return res.status(404).render('notFound',{
+    page: req.url,
+  });
 };
 
 module.exports = {
